0359fc75acc673d36c88a9c064a4a9996f64d954
Ot-A <programming.factory.2022@gmail.com>
2022-12-11 13:47:47 +0900

    initial commit

---
+++ main/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import Talk, User  # Talk を追加
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email",)
+
+class LoginForm(AuthenticationForm):
+    pass
+
+
+# ...
+
+class TalkForm(forms.ModelForm):
+    class Meta:
+        model = Talk
+        fields = ("message",)
+
+
+class UsernameChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        labels = {"username": "新しいユーザー名"}
+        help_texts = {"username": ""}
+
+
+# 以下を追加
+class EmailChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+        labels = {"email": "新しいメールアドレス"}
